bc68e1a24c95a543cffb4cde9a23f5754c6a293a
Rafael Ehammer <Rafael.ehammer@htl-wels.at>
2021-02-25 11:28:46 +0100

    implemented class Person

--- src/Model/Person.java
+++ src/Model/Person.java
@@ -2,5 +2,49 @@ package Model;
 
 public class Person
 {
+    private String name;
+    private String address;
+    private String number;
 
+    public String getAddress()
+    {
+        return address;
+    }
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public String getNumber()
+    {
+        return number;
+    }
+    public void setNumber(String number)
+    {
+        this.number = number;
+    }
+    public Person ()
+    {
+        setAddress("-");
+        setName("-");
+        setNumber("-");
+    }
+    public Person(String name, String address, String number)
+    {
+        this.name = name;
+        this.address = address;
+        this.number = number;
+    }
+    @Override
+    public String toString()
+    {
+        return name + "," + address + "," + number;
+    }
 }
